f416c64aad01f091965b85181ddc98aef94637f2
Sejal Abhangrao <sejal.abhangrao@mobiliya.com>
2018-03-14 15:53:57 +0530

    Set the base

---
+++ project1/Business/UserBiz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project1.Business
+{
+    public class UserBiz
+    {
+
+
+
+    }
+}
---
+++ project1/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace project1.Controllers
+{
+    public class UserController : ApiController
+    {
+        [Route("api/user/createUser")]
+        [HttpPost]
+        public static void createUser()
+        {
+
+        }
+    }
+}
