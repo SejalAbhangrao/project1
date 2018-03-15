7dc6d005ee9b2533de178f2e11b7fe59f0e9beb5
Sejal Abhangrao <sejal.abhangrao@mobiliya.com>
2018-03-15 18:05:24 +0530

    CRUD operations completed

--- project1/Controllers/UserController.cs
+++ project1/Controllers/UserController.cs
@@ -4,16 +4,95 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using project1.Models;
+using project1.Util;
+using project1.Business;
+using project1.Controllers;
 
 namespace project1.Controllers
 {
+
     public class UserController : ApiController
     {
-        [Route("api/user/createUser")]
-        [HttpPost]
-        public static void createUser()
-        {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
+        [Route("api/user/userSignUp")]
+        [AllowAnonymous]
+        [HttpPost]
+        public ResponseModel SignUpUser(UserModel user)
+        {
+            UserService userService = new UserService();
+            ResponseModel responseModel = new ResponseModel();
+            try
+            {
+                userService.UserSignUp(user, responseModel);
+            }
+            catch (Exception e)
+            {
+                responseModel.status = ResponseConstants.SYSTEM_ERROR;
+                log.Error(e);
+            }
+            return responseModel;
+        }
+
+        [Route("api/user/updateUser")]
+        [AllowAnonymous]
+        [HttpPost]
+        public ResponseModel UpdateUser(UserModel user)
+        {
+            UserService userService = new UserService();
+            ResponseModel responseModel = new ResponseModel();
+            try
+            {
+                userService.UpdateUser(user, responseModel);
+            }
+            catch (Exception e)
+            {
+                responseModel.status = ResponseConstants.SYSTEM_ERROR;
+                log.Error(e);
+            }
+            return responseModel;
+        }
+
+
+        [Route("api/user/deleteUser")]
+        [AllowAnonymous]
+        [HttpPost]
+        public ResponseModel DeleteUser(UserModel user)
+        {
+            UserService userService = new UserService();
+            ResponseModel responseModel = new ResponseModel();
+            try
+            {
+                userService.DeleteUser(user, responseModel);
+            }
+            catch (Exception e)
+            {
+                responseModel.status = ResponseConstants.SYSTEM_ERROR;
+                log.Error(e);
+            }
+            return responseModel;
+        }
+
+
+
+        [Route("api/user/getUser")]
+        [AllowAnonymous]
+        [HttpPost]
+        public ResponseModel RetrieveUser(UserModel user)
+        {
+            UserService userService = new UserService();
+            ResponseModel responseModel = new ResponseModel();
+            try
+            {
+                userService.RetrieveUser(user, responseModel);
+            }
+            catch (Exception e)
+            {
+                responseModel.status = ResponseConstants.SYSTEM_ERROR;
+                log.Error(e);
+            }
+            return responseModel;
         }
     }
 }
--- project1/DatabaseMgmt/UserRepository.cs
+++ project1/DatabaseMgmt/UserRepository.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using project1.Models;
 using project1.Util;
+using System.Data.Entity;
+using project1.DatabaseMgmt;
 using log4net;
 using System.Web.Http;
 
@@ -12,21 +14,21 @@ namespace project1.DatabaseMgmt
 {
     public class UserRepository
     {
-        private sejalEntities sejalDBEntities = null;
+        private sejalEntities1 sejalDBEntities = null;
 
 
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public UserRepository()
         {
-            sejalDBEntities = new sejalEntities();
+            sejalDBEntities = new sejalEntities1();
         }
 
 
-        public void CreateUserEntry(UserModel user, ResponseModel responseModel)
+        public void SignUp(UserModel user, ResponseModel responseModel)
         {
             try
             {
-                var userTable =  new User
+                var userTable = new User
                 {
                     Id = user.Id,
                     FirstName = user.FirstName,
@@ -35,7 +37,7 @@ namespace project1.DatabaseMgmt
                     Password = user.Password,
                     Mobile = user.Mobile,
                     Address = user.Address,
-                    IsDeleted = user.IsDeleted,
+                    IsDeleted = Convert.ToByte(user.IsDeleted),
                 };
                 sejalDBEntities.Users.Add(userTable);
                 sejalDBEntities.SaveChanges();
@@ -54,6 +56,113 @@ namespace project1.DatabaseMgmt
                 return;
             }
             responseModel.message = "User registered successfully.";
+        }
+
+        public void UpdateUser(UserModel user, ResponseModel responseModel)
+        {
+            try
+            {
+                User userTable = (from x in sejalDBEntities.Users where x.Id == user.Id select x).FirstOrDefault();
+                if (userTable!= null) {
+                    userTable.FirstName = user.FirstName;
+                    userTable.LastName = user.LastName;
+                    userTable.Email = user.Email;
+                    userTable.Mobile = user.Mobile;
+                    userTable.Password = user.Password;
+                    userTable.Address = user.Address;
+                    userTable.IsDeleted = Convert.ToByte(user.IsDeleted);
+                    sejalDBEntities.SaveChanges();
+                    responseModel.data = userTable;
+                    responseModel.status = ResponseConstants.SUCCESS;
+                    responseModel.message = "User modified successfully.";
+                }
+                else
+                {
+                    responseModel.message = "User id is invalid";
+                    responseModel.status = ResponseConstants.RECORD_NOT_FOUND;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //transaction.Rollback();
+                responseModel.data = null;
+                responseModel.message = "Database error";
+                responseModel.status = ResponseConstants.DATABASE_ERROR;
+                log.Error(ex);
+                return;
+            }
+            responseModel.message = "User updated successfully.";
+        }
+
+
+        public void DeleteUser(UserModel user, ResponseModel responseModel)
+        {
+            try
+            {
+                User userTable = (from x in sejalDBEntities.Users where x.Id == user.Id select x).FirstOrDefault();
+                if (userTable != null)
+                {
+                    //userTable.FirstName = user.FirstName;
+                    //userTable.LastName = user.LastName;
+                    //userTable.Email = user.Email;
+                    //userTable.Mobile = user.Mobile;
+                    //userTable.Password = user.Password;
+                    //userTable.Address = user.Address;
+                    userTable.IsDeleted = 1;
+                    sejalDBEntities.SaveChanges();
+                    responseModel.data = userTable;
+                    responseModel.status = ResponseConstants.SUCCESS;
+                    responseModel.message = "User deleted successfully.";
+                }
+                else
+                {
+                    responseModel.message = "User id is invalid";
+                    responseModel.status = ResponseConstants.RECORD_NOT_FOUND;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //transaction.Rollback();
+                responseModel.data = null;
+                responseModel.message = "Database error";
+                responseModel.status = ResponseConstants.DATABASE_ERROR;
+                log.Error(ex);
+                return;
+            }
+            responseModel.message = "User Deleted successfully.";
+        }
+
+        public void RetrieveUser(UserModel user, ResponseModel responseModel)
+        {
+            try
+            {
+                List<User> userTable = (from x in sejalDBEntities.Users where x.IsDeleted == 0 select x).ToList();
+                if (userTable != null)
+                {
+                    responseModel.data = userTable;
+                    responseModel.status = ResponseConstants.SUCCESS;
+                    responseModel.message = "User list available.";
+                    
+                }
+                else
+                {
+                    responseModel.message = "User list not available";
+                    responseModel.status = ResponseConstants.RECORD_NOT_FOUND;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //transaction.Rollback();
+                responseModel.data = null;
+                responseModel.message = "Database error";
+                responseModel.status = ResponseConstants.DATABASE_ERROR;
+                log.Error(ex);
+                return;
+            }
+            responseModel.message = "User List Retrieved successfully.";
         }
     }
 }
